4f9c846bee8b3a209a6e0bf125bd3fbaaa6cb8ad
2b3q <2bb3qq@gmail.com>
2019-02-18 15:09:09 +0300

    register interceptor logger and httpException logger dependencies at root app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+
 import { MailModule } from './mail/mail.module';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [MailModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter, // global http Exception Error handler filter
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor, // global Logger interceptor
+    },
+  ],
 })
 export class AppModule {}
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,3 @@
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -7,23 +6,10 @@ import { MailController } from './mail.controller';
 import { NewmailController } from './newmail/newmail.controller';
 import { NewmailService } from './newmail/newmail.service';
 import { Mail } from './mail.entity';
-import { HttpErrorFilter } from '../shared/http-error.filter';
-import { LoggingInterceptor } from '../shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Mail])],
-  providers: [
-    MailService,
-    NewmailService,
-    {
-      provide: APP_FILTER,
-      useClass: HttpErrorFilter, // http Error handler filter
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor, // Logger interceptor
-    },
-  ],
+  providers: [MailService, NewmailService],
   controllers: [MailController, NewmailController],
 })
 export class MailModule {}
