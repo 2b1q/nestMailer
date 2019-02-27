98cec73e8cab431a729b7d78c28b1505886c09e5
2b3q <2bb3qq@gmail.com>
2019-02-27 16:29:08 +0300

    add rpc/service ping mock endpoint

--- src/rpc/rpc.controller.ts
+++ src/rpc/rpc.controller.ts
@@ -1,14 +1,39 @@
 import { Controller, Get, Logger, Param } from '@nestjs/common';
+import {
+  Client,
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
 @Controller('rpc')
 export class RpcController {
+  // constructor() {
+  //   this.client = ClientProxyFactory.create({
+  //     transport: Transport.REDIS,
+  //     options: {
+  //       url: 'redis://localhost:6379',
+  //     },
+  //   });
+  // }
+
+  @Client({
+    transport: Transport.REDIS,
+    options: {
+      url: 'redis://localhost:6379',
+    },
+  })
+  private client: ClientProxy;
+
   @Get('ping/:service')
-  ping(@Param('service') service: string) {
+  async ping(@Param('service') service: string) {
     Logger.warn(`call method ping to service: ${service}`, 'RpcController');
 
+    const conn = await this.client.connect();
+    this.client.send({ cmd: 'ping' }, { msg: 123 });
     // mock data
     return {
-      status: '',
+      status: conn,
       service,
     };
   }
